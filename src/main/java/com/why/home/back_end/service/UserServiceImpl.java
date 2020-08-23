82d4be4d7cf6e1e956c00f52521ff53bb5f64fa0
wanghaoyang949 <wanghaoyang949@gmail.com>
2020-08-23 15:18:01 +0800

    MD5 encryption is added

--- src/main/java/com/why/home/back_end/service/UserServiceImpl.java
+++ src/main/java/com/why/home/back_end/service/UserServiceImpl.java
@@ -2,8 +2,11 @@ package com.why.home.back_end.service;
 
 import com.why.home.back_end.dao.UserRepository;
 import com.why.home.back_end.entity.User;
+import com.why.home.back_end.util.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import static com.why.home.back_end.util.MD5Utils.code;
 
 /*---------------------------------------------------------------
               UserServiceImpl Release 1.0
@@ -19,14 +22,19 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService{
 
     /*----注解 可以对类成员变量、方法及构造函数进行标注 让Spring完成bean自动装配工作---*/
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     /*------覆写定义好的接口方法-----*/
+    /*------!!!!在业务逻辑层利用code静态方法对密码进行加密 以适配数据库中已经加密的密码!!!-----*/
+
     @Override
     public User checkUser(String username, String password) {
         /*------此处实现数据库逻辑操作（DAO层），拿到用户名密码查数据库，查到返回User，查不到返回空----*/
-        User user=userRepository.findByUsernameAndPassword(username,password);
+        User user=userRepository.findByUsernameAndPassword(username,code(password));
         /*------!!!此处返回DAO层处理后的user!!!-----*/
         return user;
     }
--- src/main/java/com/why/home/back_end/util/MD5Utils.java
+++ src/main/java/com/why/home/back_end/util/MD5Utils.java
@@ -1,4 +1,54 @@
 package com.why.home.back_end.util;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/*---------------------------------------------------------------
+              MD5Utils Release 1.0
+;  Copyright (c) 2020-2020 by why Co.
+;  Written by why on 2020/8/22.
+;
+;  基于 AOP (Aspect Oriented Programming) 面向切面编程知识。
+;
+;  Function:
+;          MD5加密 用于密码防护
+;           @Param: 要加密的字符串
+;           @Return: 加密后的字符串
+;
+----------------------------------------------------------------*/
+
 public class MD5Utils {
+    /*------定义静态方法 引入后可直接使用-----*/
+    /*------用于加密的code方法 输入原始密码 输出加密字符串-----*/
+    public static String code(String str){
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(str.getBytes());
+            byte[]byteDigest = md.digest();
+            int i;
+            StringBuffer buf = new StringBuffer();
+            for(int byteD:byteDigest){
+                i=byteD;
+                if (i < 0)
+                    i += 256;
+                if (i < 16)
+                    buf.append("0");
+                buf.append(Integer.toHexString(i));
+            }
+            /*------可输出16/32位的加密后的密码-----*/
+            //32位加密
+            return buf.toString();
+            // 16位的加密
+            //return buf.toString().substring(8, 24);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /*-------------------用于测试--------------------*/
+    public static void main(String[] args) {
+        /*------打印111111的MD5加密后的字符串-----*/
+        System.out.println(code("q5967738"));
+    }
 }
